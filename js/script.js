b23174273f13453ad1f86de97a5fe2764afde593
YukiKawano <yukijuki@YukiKawanos-MacBook-Pro.local>
2020-06-25 21:31:18 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// JavaScript Document
+
+//スマホの時だけ電話番号有効化
+
+if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
+  $(function() {
+    $('.tel').each(function() {
+      var str = $(this).html();
+      if ($(this).children().is('img')) {
+        $(this).html($('<a>').attr('href', 'tel:' + $(this).children().attr('alt').replace(/-/g, '')).append(str + '</a>'));
+      } else {
+        $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
+      }
+    });
+  });
+}
+
+//トップへ戻るボタン
+
+$(function() {
+    var topBtn = $('#page-top');    
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
+  $(function() {
+	  $("#page-top").hide();
+});
+}
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+//スマホのときだけ、トップへ戻るボタン無効化
+
+if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
+  $(function() {
+	  $("#page-top").hide();
+});
+}
+
+
+//スムーススクロール
+
+$(function(){
+	$('nav a[href^="#"]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
