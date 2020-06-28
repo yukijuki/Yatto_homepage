883d03f7fedc704d099fa67471c3da3ede6ab26c
hayatespinner <white8810wnew@gmail.com>
2020-06-28 16:52:50 +0900

    animation

--- js/script.js
+++ js/script.js
@@ -84,18 +84,13 @@ $(window).on("load", function() {
 
 //スライドショー
 var swiper = new Swiper('.swiper-container', {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true
   },
 });
